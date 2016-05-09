2679eed275ba28a41d8dc6d788af981f10b133f7
MicroNanoSensor <MicroNanoSensor>
2016-05-09 21:54:46 +0800

    完成actionOpen, actionClose

--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,12 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "serialsenddialog.h"
+#include "serialsettingdialog.h"
+
+class QModbusRtuSerialMaster;
+class QModbusReply;
+class QTime;
 
 namespace Ui {
 class MainWindow;
@@ -16,17 +22,19 @@ public:
     ~MainWindow();
 
 private slots:
-
     void on_actionClose_triggered();
-
     void on_actionSend_triggered();
-
     void on_actionSetting_triggered();
-
     void on_actionOpen_triggered();
 
 private:
     Ui::MainWindow *ui;
+    SerialSendDialog *serialSendDialog;
+    SerialSettingDialog *serialSettingDialog;
+
+    QModbusRtuSerialMaster *modbusRtu;
+    QModbusReply *reply;
+    QTime *timesample;
 };
 
 #endif // MAINWINDOW_H
